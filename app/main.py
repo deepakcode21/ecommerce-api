308783fd23017738f1deea7563d8b8ecfb7ed868
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-07-20 02:15:24 +0530

    backend cheak api

--- app/main.py
+++ app/main.py
@@ -4,5 +4,9 @@ from app.routes.order_routes import router as order_router
 
 app = FastAPI(title="E-commerce API")
 
+@app.get("/")
+async def root():
+    return {"message": "Hello Buddy! Backend Working Fine!"}
+
 app.include_router(product_router)
 app.include_router(order_router)
